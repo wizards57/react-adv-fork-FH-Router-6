6f1d74feb023cb48503ce25901b67129c477017e
Fernando <wizards57@hotmail.com>
2023-02-17 20:12:33 -0600

    Lazy Basico listo React Router 6

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -4,3 +4,5 @@ export const LazyPage1 = () => {
         </>
     )
 };
+
+export default LazyPage1;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -4,3 +4,5 @@ export const LazyPage3 = () => {
         </>
     )
 };
+
+export default LazyPage3;
